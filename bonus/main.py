e28e4bd0596e7abf3a588ca224b6522ae1d46947
fredrukundo <dukefred9@gmail.com>
2026-01-31 18:54:31 +0100

    bonus

--- bonus/main.py
+++ bonus/main.py
@@ -1,5 +1,5 @@
 import sys
-from parser import parse_side, validate_equation
+from parser import parse_side, validate_equation, insert_implicit_mul
 from reducer import reduce_equation, format_reduced, polynomial_degree
 from solver import solve_degree_0, solve_degree_1, solve_degree_2
 
@@ -7,7 +7,7 @@ def main():
 
     try:
         if len(sys.argv) != 2:
-            print("Usage: python3 computor.py \"equation\"")
+            print("Usage: python3 main.py \"equation\"")
             return
         equation = sys.argv[1]
         
@@ -15,6 +15,13 @@ def main():
         print("Parsing equation...")
         left_expr, right_expr = equation.split("=")
 
+        if not left_expr:
+            print("invalid format")
+            return
+
+        left_expr = insert_implicit_mul(left_expr)
+        right_expr = insert_implicit_mul(right_expr)
+        
         left = parse_side(left_expr)
         right = parse_side(right_expr)
 
@@ -36,16 +43,16 @@ def main():
 
         if degree == 1:
             print("Polynomial degree: 1")
-            a = reduced[1]
-            b = reduced[0]
+            a = reduced.get(1, 0)
+            b = reduced.get(0, 0)
             print(solve_degree_1(a, b))
             return
 
         if degree == 2:
             print("Polynomial degree: 2")
-            a = reduced.get(2, 0.0)
-            b = reduced.get(1, 0.0)
-            c = reduced.get(0, 0.0)
+            a = reduced.get(2, 0)
+            b = reduced.get(1, 0)
+            c = reduced.get(0, 0)
             print(solve_degree_2(a, b, c))
 
     except ValueError as e:
